16651e4660d091a157a8ec20d2390c0bd93f6185
George Baugh <george@troglodyne.net>
2024-08-14 00:56:44 +0000

    Update for new seleniums

--- lib/Selenium/Driver/SeleniumHQ/Jar.pm
+++ lib/Selenium/Driver/SeleniumHQ/Jar.pm
@@ -14,6 +14,7 @@ use File::Path     qw{make_path};
 use File::Spec();
 use XML::LibXML();
 use HTTP::Tiny();
+use Github::ReleaseFetcher;
 
 #ABSTRACT: Download the latest version of seleniumHQ's selenium.jar, and tell Selenium::Client how to spawn it
 
@@ -21,6 +22,8 @@ use HTTP::Tiny();
 
 Downloads the latest Selenium JAR (or the provided driver_version).
 Expects java to already be installed.
+
+Can find both the new releases on github and the old releases on google storage.
 
 Spawns a selnium server on the provided port (which the caller will assign randomly)
 Pipes log output to ~/.selenium/perl-client/$port.log
@@ -42,7 +45,18 @@ sub build_spawn_opts ( $class, $object ) {
     $object->{driver_interpreter} //= 'java';
     $object->{driver_version}     //= '';
     $object->{log_file}           //= File::Spec->catfile( $object->{client_dir}, "perl-client", "selenium-$object->{port}.log" );
-    ( $object->{driver_file}, $object->{driver_major_version} ) = find_and_fetch( File::Spec->catdir( $object->{client_dir}, "jars" ), $object->{driver_version}, $object->{ua} );
+
+    # Default to new selenium versions, but allow fetching old stuff
+    $object->{driver_major_version} = 4;
+    ($object->{driver_major_version}) = $object->{driver_version} =~ m/-(\d+)\.\d+\.\d.*\.jar$/ if $object->{driver_version};
+
+    die "Could not determine driver major version!" unless $object->{driver_major_version};
+
+    if ( $object->{driver_major_version} < 4 ) {
+        ( $object->{driver_file}, $object->{driver_major_version} ) = find_and_fetch_old_releases( File::Spec->catdir( $object->{client_dir}, "jars" ), $object->{driver_version}, $object->{ua} );
+    } else {
+        ( $object->{driver_file}, $object->{driver_major_version} ) = find_and_fetch( File::Spec->catdir( $object->{client_dir}, "jars" ), $object->{driver_version}, $object->{ua} );
+    }
     $object->{driver_config} //= _build_config($object);
 
     #XXX port in config is currently IGNORED
@@ -110,9 +124,23 @@ sub _build_config ($self) {
 Does an index lookup of the various selenium JARs available and returns either the latest one
 or the version provided.  Stores the JAR in the provided directory.
 
+Also fetches any versions we don't already have.
+
 =cut
 
-sub find_and_fetch ( $dir, $version = '', $ua = '' ) {
+sub find_and_fetch ( $dir, $version = undef, $ua=undef ) {
+    make_path($dir) unless -d $dir;
+
+    $version ||= undef;
+
+    my @files = Github::ReleaseFetcher::fetch($dir, 'SeleniumHQ', 'selenium', qr/\.jar$/, undef, $version, $ua );
+    my $latest_jar_at_version = pop(@files);
+    my ($actual_version) = $latest_jar_at_version =~ m/-(\d+)\.\d+\.\d.*\.jar$/;
+
+    return ( $latest_jar_at_version, $actual_version );
+}
+
+sub find_and_fetch_old_releases ( $dir, $version = '', $ua = '' ) {
     $ua ||= HTTP::Tiny->new();
     my $res = $ua->get($index);
     confess "$res->{reason} :\n$res->{content}\n" unless $res->{success};
@@ -132,7 +160,7 @@ sub find_and_fetch ( $dir, $version = '', $ua = '' ) {
 
     make_path($dir) unless -d $dir;
     my $fname = File::Spec->catfile( $dir, basename($jar) );
-    my ($v) = $fname =~ m/-(\d)\.\d\.\d.*\.jar$/;
+    my ($v) = $fname =~ m/-(\d+)\.\d+\.\d.*\.jar$/;
     return ( $fname, $v ) if -f $fname;
 
     $res = $ua->mirror( $url, $fname );
